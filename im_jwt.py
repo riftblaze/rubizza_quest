f12107f1b9e73ce9d03b024c939173fc2cd0e1b5
Yta <riftblaze@users.noreply.github.com>
2019-07-18 18:32:04 +0300

    Add files via upload
    Улучшено, не ругается ошибками дживити

--- im_jwt.py
+++ im_jwt.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python encoding='utf-8'
-#это не работает. но общий смысл такой)
+import json
 import jwt
-#воспользуемся либой, которую умные люди сделали до нас
-import os
-key = "yours_mail@abc.de"
-#по условию задачи
-payload_file = open('payload.json','r')
-#в нагрузке данные сиви
-encoded = jwt.encode(payload_file, key, algorithm='HS256')
+
+key = 'yours_mail@abc.de'
+data = '''{        "age": 32,
+         "english": "intermediate",
+         "full_name": "Taisiia Alekseevna Ialovaia",
+         "goal": "improve ruby skills",
+         "education": "SPbGUT,2021,applied cybersecurity",
+         "have_a_job": "no",
+         "hours_per_day": 13,
+         "programming_languages": "Python",
+         "laptop": "yes",
+         "phone_number": 79500153410 }'''
+
+#print(json.dumps(data,sort_keys=True,indent=4,separators=('\n', ':')))
+#print('~')
+#print("data " + str(type(data))) - класс строка
+print('~ this will create a token for rubizza quest')
+print(data)
+print('~')
+payload = open('payload.json', 'w')
+payload.write(data)
+payload.close()
+print("~")
+#payload = open('payload.json', 'r')
+#print ('~')
+#readdata = payload.read()
+#print(readdata)
+#payload.close()
+
+encoded = jwt.encode(json.load(open('payload.json', 'r')), key, algorithm='HS256')
 print(encoded)
-#отсюда мы получим нужный нам "токен"
-payload_file.close()
-#закрыть процесс с файлом
+
+token = open('token', 'wb')
+token.write(encoded)
+token.close()
+
+
